b386d9688ba9794560a72a8176fe90d7a51a5606
bojorquezja <bojorquez.ja@gmail.com>
2024-10-24 09:25:02 -0500

    first commit

---
+++ arreglos/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] edificio1 = new string[5];
+for(int x=0; x<edificio1.Length; x++) {
+    Console.WriteLine($"Ingrese nombre {x+1}:");
+    edificio1[x] = Console.ReadLine();
+}
+
+for (int x = 0; x < edificio1.Length; x++) {
+    Console.WriteLine(edificio1[x]);
+}
+
+Console.WriteLine();
+
+string[] edificio2 = { "tu", "papas","hermano", "tio", "primo" };
+foreach (string e in edificio2) {
+    Console.WriteLine(e);
+}
+
+//Console.WriteLine(edificio1[5]);    //error por no existir
+
+Console.WriteLine("A quien busca en edificio 2:");
+string bus = Console.ReadLine().ToLower();
+for (int x = 0; x < edificio2.Length; x++) {
+    if (edificio2[x] == bus) {
+        Console.WriteLine($"Está en el piso {x+1}");
+        break;
+    }
+}
